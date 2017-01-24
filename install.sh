1fb277e8d7fb95a2e649985f945da3ce790ea706
sevaho <sebastiaan.vanhoecke@hotmail.be>
2017-01-24 22:18:23 +0100

    git quick push

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 
-# Author: Sebastiaan Van Hoecke
+#Author: Sebastiaan Van Hoecke
 
 main(){
-git clone https://github.com/sevaho/dotFiles.git ~/dotFiles
-cp -rf ~/dotfiles/.* ~/
-rm -rf ~/dotFiles
+  git clone https://github.com/sevaho/dotFiles.git ~/dotFiles
+  cp -rf ~/dotfiles/.* ~/
+  rm -rf ~/dotFiles
 }
 
-#functions
 main "${@}"
 
