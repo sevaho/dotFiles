0ee28846e200100bcbc72fec14426af6507db0b7
sevaho <sebastiaan.vanhoecke@hotmail.be>
2017-01-29 21:55:37 +0100

    update install and added termite

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ check_installed_software(){
 
 install_configs(){
 
-  rm -vrf ~/.dotFiles
-  rm -vrf ~/.fonts
+  rm -ivr ~/.dotFiles
+  rm -ivr ~/.fonts
 
   env git clone --depth=1 https://github.com/sevaho/dotFiles.git ~/.dotFiles || {
     printf "Error: git clone of dotFiles\n"
@@ -31,29 +31,22 @@ install_configs(){
     exit 1
   }
 
-  cp -vrf ~/.dotFiles/.conkyrc ~/
-  cp -vrf ~/.dotFiles/.bashrc ~/
-  cp -vrf ~/.dotFiles/.tmux.conf ~/
-  #cp -vrf ~/.dotFiles/.oh-my-zsh ~/
-  #cp -vrf ~/.dotFiles/.vim ~/
-  cp -vrf ~/.dotFiles/.vimrc ~/
-  cp -vrf ~/.dotFiles/.zshrc ~/
-  cp -vrf ~/.dotFiles/.notes ~/
-  cp -rf ~/.dotFiles/.gitignore_global ~/
-  #cp -rf ~/.dotFiles/. ~/
-
-  ~/.fonts/install.sh
-  fc-cache -f -v
-
   #VIM plugins
 
-  git clone https://github.com/jeremyFreeAgent/oh-my-zsh-powerline-theme.git
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
   curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
   apt-vim install -y https://github.com/scrooloose/nerdtree.git
-  git clone git://github.com/jiangmiao/auto-pairs.git ~/.vim/bundle/auto-pairs 
+  apt-vim install -y https://github.com/jiangmiao/auto-pairs.git
+  apt-vim install -y https://github.com/vim-airline/vim-airline.git
+  apt-vim install -y https://github.com/vim-airline/vim-airline-themes.git
+  apt-vim install -y https://github.com/pangloss/vim-javascript.git
+  apt-vim install -y https://github.com/digitaltoad/vim-pug.git
+  apt-vim install -y https://github.com/mhinz/vim-signify.git
+  apt-vim install -y https://github.com/vim-syntastic/syntastic.git
+  apt-vim install -y https://github.com/Valloric/YouCompleteMe.git
 
   #ZSH plugins
+
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
 
   env git clone --depth=1 git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions || {
     printf "Error: git clone of zsh autosuggest\n"
@@ -64,6 +57,22 @@ install_configs(){
     printf "Error: git clone of zsh syntax highlight\n"
     exit 1
   }
+
+  cp -vrf ~/.dotFiles/.conkyrc ~/
+  cp -vrf ~/.dotFiles/.bashrc ~/
+  cp -vrf ~/.dotFiles/.tmux.conf ~/
+  cp -vrf ~/.dotFiles/.oh-my-zsh/themes/agnoster.zsh-theme ~/.oh-my-zsh/themes/
+  cp -vrf ~/.dotFiles/.vim/colors ~/.vim/
+  cp -vrf ~/.dotFiles/.vim/plugin ~/.vim/
+  cp -vrf ~/.dotFiles/.vim/ftdetect ~/.vim/
+  cp -vrf ~/.dotFiles/.vimrc ~/
+  cp -vrf ~/.dotFiles/.zshrc ~/
+  cp -vrf ~/.dotFiles/.notes ~/
+  cp -rf ~/.dotFiles/.gitignore_global ~/
+  #cp -rf ~/.dotFiles/. ~/
+
+  ~/.fonts/install.sh
+  fc-cache -f -v
 
   echo "source .zshrc"
   sleep 5
