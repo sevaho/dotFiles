b4a7896c78135df606752df4484c0709f5732555
sevaho <sebastiaan.vanhoecke@hotmail.be>
2017-11-10 14:40:55 +0100

    cleaned i3 directory, modified colors termite

--- .config/i3/i3lock.sh
+++ .config/i3/i3lock.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-# if [[ $WAYLAND_DISPLAY == "" ]]; then
-#
-# 	scrot /tmp/screen.png
-#
-# else  
-
-	# swaygrab /tmp/screen.png
-
-# fi
+scrot /tmp/screen.png
 
 convert /tmp/screen.png -paint 1 /tmp/screen.png
 [[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.config/i3/lock.png -gravity Center -composite -matte /tmp/screen.png
 
-
-# if [[ $WAYLAND_DISPLAY == "" ]]; then
-#
-# 	i3lock -u -e -i /tmp/screen.png
-#
-# else  
-
-	swaylock -u -i /tmp/screen.png
-
-# fi
+i3lock -u -e -i /tmp/screen.png
