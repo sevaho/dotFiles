12dcc640dfaa9f300304831f550cbf18b326fd05
sevaho <sebastiaan@sevaho.io>
2024-07-25 13:16:32 +0200

    Add more

--- .config/i3/i3lock.sh
+++ .config/i3/i3lock.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-scrot /tmp/screen.png
+# scrot /tmp/screen.png
+#
+# convert /tmp/screen.png -paint 1 /tmp/screen.png
+# [[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.config/i3/lock.png -gravity Center -composite -matte /tmp/screen.png
+#
+# i3lock -efi /tmp/screen.png
+
+# notify-send "DUNST_COMMAND_PAUSE"
+
+scrot -o /tmp/screen.png
 
 convert /tmp/screen.png -paint 1 /tmp/screen.png
-[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.config/i3/lock.png -gravity Center -composite -matte /tmp/screen.png
+# [[ -f ~/.config/i3/circlelock.png ]] && convert /tmp/screen.png ~/.config/i3/circlelock.png -gravity Center -composite -matte /tmp/screen.png
+# convert /tmp/screen.png ~/.config/i3/circlelock.png -gravity center -composite /tmp/screen.png
 
+# i3lock -efi /tmp/screen.png
+
+# forget gpg passwords
+# gpgconf --reload gpg-agent
+
+# i3lock --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c --radius=35 --veriftext="" -nfi /tmp/screen.png
 i3lock -efi /tmp/screen.png
+
+# notify-send "DUNST_COMMAND_RESUME"
--- .offlineimap.py
+++ .offlineimap.py
@@ -3,4 +3,4 @@
 from subprocess import check_output
 
 def get_pass(account):
-    return check_output("pass " + account, shell=True).splitlines()[0]
+    return check_output("pass " + account, shell=True).splitlines()[0].decode()
